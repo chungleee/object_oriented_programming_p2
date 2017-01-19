7d6b33ef3f1eaba78b9e6cfad1dec9ca9ac43aeb
Leon <leon.karho.chung@gmail.com>
2017-01-19 14:50:49 -0500

    final exercise1

--- people.rb
+++ people.rb
@@ -1,10 +1,15 @@
 # create parent Person class
 class Person
   # initialize
+  attr_accessor :name
+
   def initialize(name)
     @name = name
   end
 
+  def greetings
+    "Hi, my name is #{name}"
+  end
 end
 
 
@@ -14,6 +19,7 @@ end
 # create student and instructor classes
 class Student < Person
   # defining methods
+
   def learn
     "I get it!"
 
@@ -33,3 +39,7 @@ class Instructor < Person
   end
 
 end
+
+# the teach method doesn't work on the student instance
+# because the method isn't within its' class
+# it would've worked though if it was part of the parent class
